727f39181214daba3b80881b54a2e3f2489069e4
Intern097-17 <62545643+Intern097-17@users.noreply.github.com>
2020-06-12 10:40:54 +0200

    Update Sprint 2 #1.py

--- Sprint 2 #1.py
+++ Sprint 2 #1.py
@@ -9,7 +9,7 @@
 
 # In[ ]:
 
-
+# Dice Rolling Simulator
 import random
 
 question = input('Would you like to roll the dice [Y/N]?\n')
@@ -19,9 +19,9 @@ while question != 'N':
         die1 = random.randint(1, 6)
         die2 = random.randint(1, 6)
         print(die1, die2)
-        question = input('Would you like to roll the dice [Y/N]?\n')
+        question = input('Would you like to roll the dice [Y/N]?\n') # User then chooses between Yes and No
     else:
-        print('Invalid response. Please type "Y" or "N".')
+        print('Invalid response. Please type "Y" or "N".') # If hypothetically user types a letter/digit thats not N or Y
         question = input('Would you like to roll the dice [Y/N]?\n')        
 
 print('Good-bye!')
